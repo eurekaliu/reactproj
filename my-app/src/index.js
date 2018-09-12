0eb8d1cc4f4a947b63d6e5cc4c08c53dfab2e091
chouni0613 <chouni0613@gmail.com>
2018-09-12 12:09:14 +0800

    modify

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,7 @@
 //import React from 'react';
 //import ReactDOM from 'react-dom';
-//import './App.js';
+import './index.css';
+import './App.js';
 //import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
